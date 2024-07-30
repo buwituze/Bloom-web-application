7b7920e5745fc5b1b7596cc4bccaff99b3ba2beb
buwituze <b.rutagengw@alustudent.com>
2024-07-30 16:02:44 +0200

    loginupdate

--- Bloom-project/Front-end/src/components/login.tsx
+++ Bloom-project/Front-end/src/components/login.tsx
@@ -4,6 +4,45 @@ import NavBar from "./Navbar";
 import { useNavigate } from "react-router-dom";
 import "../styles/login.css";
 
+// function Login() {
+//   return (
+//     <div>
+//       <NavBar />
+//       <div className="login-container">
+//         <div>
+//           <img className="login-image" src="FARMER.jpg"></img>
+//         </div>
+//         <div className="login">
+//           <h3>Login</h3>
+//           <form>
+//             <div>
+//               <label htmlFor="username">Username</label> <br />
+//               <input type="text" id="username" required /> <br /> <br />
+//             </div>
+//             <div>
+//               <label htmlFor="email">Email</label> <br />
+//               <input type="email" id="email" required /> <br /> <br />
+//             </div>
+//             <div>
+//               <label htmlFor="password">Password</label> <br />
+//               <input type="password" id="password" required /> <br /> <br />
+//             </div>
+//             <div>
+//               <Link to="/homepage">
+//                 <button type="submit">Sign in</button>
+//               </Link>
+//             </div>
+//             <p>
+//               Don't have an account? <Link to="/signup">Sign up</Link>
+//             </p>
+//           </form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+// export default Login;
+
 const Login: React.FC = () => {
   const [username, setUsername] = useState<string>("");
   const [email, setEmail] = useState<string>("");
@@ -65,16 +104,17 @@ const Login: React.FC = () => {
           {error && <p className="error">{error}</p>}
           <form onSubmit={handleSubmit}>
             <div>
-              <label htmlFor="email">Email</label> <br />
+              <label htmlFor="username">Username</label> <br />
               <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
                 required
               />{" "}
               <br /> <br />
             </div>
+
             <div>
               <label htmlFor="password">Password</label> <br />
               <input
