5a2ffbb5955a9bd45a154d3d977fe6881f76098a
buwituze <b.rutagengw@alustudent.com>
2024-08-01 11:00:23 +0200

    cart&resources

--- Bloom-project/Front-end/src/components/resources.tsx
+++ Bloom-project/Front-end/src/components/resources.tsx
@@ -26,7 +26,7 @@ const Resources: React.FC = () => {
       <FarmerNavbar />
       <section className="resources">
         <div className="intro">
-          <img src="/resourcesimage.png" alt="" />
+          <img src="/resourcepageimage.jpg" alt="" />
         </div>
         <div className="facts">
           <div className="fact-card">
